4c4507150c9018d798b30e9fceedff47d98b8eda
Dev <rishabsing2990@gmail.com>
2025-08-27 15:18:58 +0530

    Traversal program

---
+++ Traversal.cpp
@@ -0,0 +1,9 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+     int arr[10]={1,3,5,6,7,8,9,10,4,6};
+     for (int i=0; i<10; i++){
+         cout<<arr[i]<<endl;
+     }
+     return 0;
+}
